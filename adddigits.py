9d593625266f4650c5ac2bcafed6a2e759871496
Taha <mohdtaha.hakim@gmail.com>
2026-09-12 23:22:09 +0400

    Add digits

---
+++ adddigits.py
@@ -0,0 +1,13 @@
+class Solution:
+    def addDigits(self, num: int) -> int:
+        n = num
+        if len(str(num))==1:
+            return num
+        total = 0 
+        while n>0:
+            n,digit = divmod(n,10)
+            total+=digit
+        return self.addDigits(total)
+
+test = Solution()
+print(test.addDigits(38))
