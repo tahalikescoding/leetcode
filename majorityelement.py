728da1284a47748557786a91cda327586386af27
Taha <mohdtaha.hakim@gmail.com>
2026-09-13 17:40:23 +0400

    majority element

---
+++ majorityelement.py
@@ -0,0 +1,12 @@
+#LEETCODE 169 : MAJORITY ELEMENT WITH O(1) SPACE AND O(N) TIME
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        count = 0 
+        candidate = None
+        for num in nums:
+            if count == 0 :
+                candidate = num 
+            count+= 1 if candidate == num else -1
+        return candidate
+        
