56b2f6af80fd96c9a41ded3cbf59e1c613d42b52
Taha <mohdtaha.hakim@gmail.com>
2026-09-13 21:25:29 +0400

    Power of two

---
+++ poweroftwo.py
@@ -0,0 +1,9 @@
+#LEETCODE 231: POWER OF TWO
+
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        if n<=0:
+            return False
+        while n%2==0:
+            n = n//2
+        return n==1
