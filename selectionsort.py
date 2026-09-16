3b910011190e559f62aaec9177133d8d5b1c669c
Taha <mohdtaha.hakim@gmail.com>
2026-09-16 11:25:17 +0400

    insertion

--- selectionsort.py
+++ selectionsort.py
@@ -32,3 +32,16 @@ def bubbleasc(arr):
     return arr
 
 print(bubbleasc([5,7,8,4,1,6,9,2]))
+
+
+def insertionasc(arr):
+    for i in range(1,len(arr)):
+        key = arr[i]
+        j = i-1 
+        while j>=0 and arr[j]>key:
+            arr[j+1] = arr[j]
+            j-=1
+        arr[j+1] = key 
+    return arr
+
+print(insertionasc([5,7,8,4,1,6,9,2]))
